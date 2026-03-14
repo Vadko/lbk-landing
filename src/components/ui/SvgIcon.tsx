e7e1142671ae8dd3e2095535146e0337bdce6dab
Vadko <vpoglid@gmail.com>
2026-03-14 23:47:01 +0200

    fix inlining

--- src/components/ui/SvgIcon.tsx
+++ src/components/ui/SvgIcon.tsx
@@ -16,7 +16,7 @@ export function SvgIcon({ icon, className }: SvgIconProps) {
       fill="currentColor"
       aria-hidden="true"
       className={className}
-      style={{ width: "1em", height: "1em" }}
+      style={{ width: "1em", height: "1em", display: "inline-block", verticalAlign: "-0.125em" }}
     >
       {typeof path === "string" ? (
         <path d={path} />
